093dfdb116bbc7f65330dd0b10949ae6ac68e6f4
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-06-05 11:42:21 +0200

    update dagger to beta 3
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,8 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   dotenv.enable = true;
 
   dagger.enable = true;
-  env.DAGGER_X_RELEASE = "v1.0.0-beta.2";
+  env.DAGGER_X_RELEASE = "v1.0.0-beta.3";
+
+  languages = {
+    dang = {
+      package = (
+        pkgs.dang.overrideAttrs (old: {
+          src = pkgs.fetchFromGitHub {
+            owner = "vito";
+            repo = "dang";
+            rev = "0a11c1e282de2d3c1bb842c96a637c3fc6ba6f4e";
+            hash = "sha256-dIRbz+Z5581Ysv6C9nEcmAZG8f4bHev1uV/pbhGK8BU=";
+          };
+        })
+      );
+    };
+  };
 }

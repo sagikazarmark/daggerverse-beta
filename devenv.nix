5fccadd6cf15f92d308ddba678aa54e7adbabceb
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-06-14 00:08:45 +0200

    devenv
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

--- devenv.nix
+++ devenv.nix
@@ -1,23 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
   dotenv.enable = true;
 
   dagger.enable = true;
-  env.DAGGER_X_RELEASE = "v1.0.0-beta.3";
-
-  languages = {
-    dang = {
-      package = (
-        pkgs.dang.overrideAttrs (old: {
-          src = pkgs.fetchFromGitHub {
-            owner = "vito";
-            repo = "dang";
-            rev = "0a11c1e282de2d3c1bb842c96a637c3fc6ba6f4e";
-            hash = "sha256-dIRbz+Z5581Ysv6C9nEcmAZG8f4bHev1uV/pbhGK8BU=";
-          };
-        })
-      );
-    };
-  };
+  env.DAGGER_X_RELEASE = "d87b131f050764cac6205aa68b51b189aeaf000b";
 }

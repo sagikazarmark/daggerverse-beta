e1ff8e372be2dbb74a05cf56f3b0efadba7b3a56
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2026-07-02 19:24:47 +0200

    add javascript environments
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ javascript-environment/tests/testdata/tailwind/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    rsx! {
+        main { class: "min-h-screen bg-base-100 text-base-content p-4",
+            button { class: "btn btn-primary rounded-box", "Sign in" }
+        }
+    }
+}
